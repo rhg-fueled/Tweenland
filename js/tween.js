2189a79f8767b9d47ece40ce0b04ee67dca2fa06
rhg-fueled <rhg@fueled.co>
2013-07-26 17:28:56 +0530

    First attempt to analyse the data - varied the range of genetic bit difference

--- js/tween.js
+++ js/tween.js
@@ -83,6 +83,111 @@ function compareArrays(source, target, print) {
 
 function main()
 {
+
+	filename = "tweens600.txt";
+	var a = loadFileIntoArray(filename);
+	var _count = 0 ;
 	
+	 var max = 0 ; 
+	 var min = 1000 ;
+	
+	// document.write("Difference in genetic bits: </br>" );
+	
+	// for(var i=0; i<600; i++)
+	// {
+	// 	var d = compareArrays(a[0], a[i]);
+	// 	//document.write(d.length);
+	// 	document.write( "( 0,  " + i + " )" + " : " + d.length + "</br>");
+	// }
+
+		// for (var i=0; i<600; i++)
+		// {
+		// 	for (var j=i+1; j<600; j++)
+		// 	{
+				
+		// 		if( max < ( compareArrays(a[i], a[j]) ).length)
+		// 			{
+		// 				max = compareArrays(a[i], a[j]).length;
+		// 				//document.write(max);
+		// 			}
+
+		// 		if (min > (compareArrays( a[i], a[j]) ).length)
+		// 			{
+		// 				min = compareArrays( a[i], a[j] ).length;
+		// 				//document.write(min);
+		// 			}
+		// 	}
+		// }
+		
+		// document.write("max: " + max + " , " + " min: " + min);
+	
+	var _upperLimit = 150;
+	var _lowerLimit = 90;
+	var _sum = 0;
+
+	document.write("</br>Genetic bit difference between : " + _upperLimit + " & " + _lowerLimit + "</br>");
+	
+	for (var i=0; i<600; i++)
+	{
+		for (var j=i; j<600; j++)
+		{
+			var _diff = compareArrays( a[i], a[j] ).length;
+
+			if( _diff >= _lowerLimit  &&  _diff <= _upperLimit )
+				{
+					document.write(i + " --> " + j + "   :   " + _diff + " (-) </br>");
+					_count++;
+					_sum += _diff;
+				} 
+		}
+		
+		if (_count != 0)  //  so that it doesn't display NaN
+		{
+			var _avg = _sum/_count;
+			document.write("Average : " + _avg + "</br>" + "</br >");
+		}	
+		_count = 0;
+		_sum = 0;
+
+	}
+
+	// var _avg = _sum/_count;
+	// document.write("</br> Count:  " + _count + "</br>");
+	// document.write("</br> Average: " + _avg);
+
 	return 0;
 }
+
+main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
